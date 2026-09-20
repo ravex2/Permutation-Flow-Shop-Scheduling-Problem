474a378c8519ab9dd8b694f4eb8b01c76e5f5122
matt <matt@gmail.com>
2026-09-20 00:23:58 -0300

    add: separando funciones en genetico y memetico

--- src/ag_build.py
+++ src/ag_build.py
@@ -4,19 +4,17 @@ import random
 from src.pfsp import fitness_pfsp
 
 ''' 
-Implementacion del algoritmo genetico para el problema PFSP
+Implementacion de Metaheuristicas para PFSP: Algoritmo Genetico Puro y Algoritmo Memetico
 '''
 
 def crear_poblacion(cantidad_trabajos, tam_pob, generador):
     poblacion = []
     plantilla = np.arange(cantidad_trabajos, dtype=np.int64)
-    print(plantilla)
     for _ in range(tam_pob):
         individuo = plantilla.copy()
         individuo = np.array(generador.sample(individuo.tolist(), cantidad_trabajos))
         poblacion.append(individuo)
     return poblacion
-
 
 def seleccion_torneo(poblacion, tiempos, generador, tam_torneo=3):
     participantes = generador.sample(
@@ -24,7 +22,6 @@ def seleccion_torneo(poblacion, tiempos, generador, tam_torneo=3):
         min(tam_torneo, len(poblacion)),
     )
     return min(participantes, key=lambda individuo: fitness_pfsp(individuo, tiempos))
-
 
 def cruce_ordenado(primer_padre, segundo_padre, generador):
     inicio, fin = sorted(generador.sample(range(len(primer_padre)), 2))
@@ -41,7 +38,6 @@ def cruce_ordenado(primer_padre, segundo_padre, generador):
             posicion += 1
     return hijo
 
-
 def mutar_intercambio(individuo, por_mul, generador):
     if generador.random() < por_mul:
         primera, segunda = generador.sample(range(len(individuo)), 2)
@@ -49,64 +45,6 @@ def mutar_intercambio(individuo, por_mul, generador):
             individuo[segunda],
             individuo[primera],
         )
-
-
-# remplazo para la nueva generacion
-# busquedas local
-
-def ejecutar_algoritmo_genetico(
-    tiempos,
-    tam_pob,
-    por_cru,
-    por_mul,
-    num_ite,
-    semilla,
-    frecuencia_local=0,
-    max_evaluaciones_local=100,
-):
-    generador = random.Random(semilla)
-    poblacion = crear_poblacion(len(tiempos), tam_pob, generador)
-    mejor = min(poblacion, key=lambda individuo: fitness_pfsp(individuo, tiempos))
-
-    for generacion in range(num_ite):
-        nueva_poblacion = [mejor.copy()]
-        while len(nueva_poblacion) < tam_pob:
-            primer_padre = seleccion_torneo(poblacion, tiempos, generador)
-            segundo_padre = seleccion_torneo(poblacion, tiempos, generador)
-
-            if generador.random() < por_cru:
-                hijo = cruce_ordenado(primer_padre, segundo_padre, generador)
-            else:
-                hijo = primer_padre.copy()
-
-            mutar_intercambio(hijo, por_mul, generador)
-            nueva_poblacion.append(hijo)
-
-        poblacion = nueva_poblacion
-        candidato = min(
-            poblacion,
-            key=lambda individuo: fitness_pfsp(individuo, tiempos),
-        )
-        if frecuencia_local and (generacion + 1) % frecuencia_local == 0:
-            mejorado = busqueda_local_insercion(
-                candidato, tiempos, generador, max_evaluaciones_local
-            )
-            indice = None
-            for i, individuo in enumerate(poblacion):
-                if individuo is candidato:
-                    indice = i
-                    break 
-
-
-            poblacion[indice] = mejorado
-            candidato = mejorado
-        if fitness_pfsp(candidato, tiempos) < fitness_pfsp(mejor, tiempos):
-            mejor = candidato.copy()
-
-    return mejor, fitness_pfsp(mejor, tiempos)
-
-
-
 
 def busqueda_local_insercion(individuo, tiempos, generador, max_evaluaciones=100):
     """Primera mejora por insercion, limitada por evaluaciones de vecinos."""
@@ -140,13 +78,116 @@ def busqueda_local_insercion(individuo, tiempos, generador, max_evaluaciones=100
     return mejor
 
 
-def ejecutar_algoritmo_memetico(
-    tiempos, tam_pob, por_cru, por_mul, num_ite, semilla,
-    generacion=1, max_gen_evaluada=100,
+def ejecutar_algoritmo_genetico(
+    tiempos,
+    tam_pob,
+    por_cru,
+    por_mul,
+    num_ite,
+    semilla,
+    retornar_historia=False,
 ):
-    if generacion < 1:
-        raise ValueError("La frecuencia local debe ser al menos 1.")
-    return ejecutar_algoritmo_genetico(
-        tiempos, tam_pob, por_cru, por_mul, num_ite, semilla,
-        generacion, max_gen_evaluada,
-    )
+    """Algoritmo Genetico Puro (exploracion sin busqueda local)."""
+    generador = random.Random(semilla)
+    cantidad_trabajos = tiempos.shape[1]
+    poblacion = crear_poblacion(cantidad_trabajos, tam_pob, generador)
+
+    mejor = min(poblacion, key=lambda individuo: fitness_pfsp(individuo, tiempos))
+    mejor_costo = fitness_pfsp(mejor, tiempos)
+
+    historia = [mejor_costo] if retornar_historia else None
+
+    for generacion in range(num_ite):
+        nueva_poblacion = [mejor.copy()]
+        while len(nueva_poblacion) < tam_pob:
+            primer_padre = seleccion_torneo(poblacion, tiempos, generador)
+            segundo_padre = seleccion_torneo(poblacion, tiempos, generador)
+
+            if generador.random() < por_cru:
+                hijo = cruce_ordenado(primer_padre, segundo_padre, generador)
+            else:
+                hijo = primer_padre.copy()
+
+            mutar_intercambio(hijo, por_mul, generador)
+            nueva_poblacion.append(hijo)
+
+        poblacion = nueva_poblacion
+        candidato = min(
+            poblacion,
+            key=lambda individuo: fitness_pfsp(individuo, tiempos),
+        )
+        candidato_costo = fitness_pfsp(candidato, tiempos)
+        if candidato_costo < mejor_costo:
+            mejor = candidato.copy()
+            mejor_costo = candidato_costo
+
+        if retornar_historia:
+            historia.append(mejor_costo)
+
+    if retornar_historia:
+        return mejor, mejor_costo, historia
+    return mejor, mejor_costo
+
+
+def ejecutar_algoritmo_memetico(
+    tiempos,
+    tam_pob,
+    por_cru,
+    por_mul,
+    num_ite,
+    semilla,
+    frecuencia_local=1,
+    max_evaluaciones_local=100,
+    retornar_historia=False,
+):
+    """Algoritmo Memetico (Algoritmo Genetico + Busqueda Local por Insercion)."""
+    generador = random.Random(semilla)
+    cantidad_trabajos = tiempos.shape[1]
+    poblacion = crear_poblacion(cantidad_trabajos, tam_pob, generador)
+
+    mejor = min(poblacion, key=lambda individuo: fitness_pfsp(individuo, tiempos))
+    mejor_costo = fitness_pfsp(mejor, tiempos)
+
+    historia = [mejor_costo] if retornar_historia else None
+
+    for generacion in range(num_ite):
+        nueva_poblacion = [mejor.copy()]
+        while len(nueva_poblacion) < tam_pob:
+            primer_padre = seleccion_torneo(poblacion, tiempos, generador)
+            segundo_padre = seleccion_torneo(poblacion, tiempos, generador)
+
+            if generador.random() < por_cru:
+                hijo = cruce_ordenado(primer_padre, segundo_padre, generador)
+            else:
+                hijo = primer_padre.copy()
+
+            mutar_intercambio(hijo, por_mul, generador)
+            nueva_poblacion.append(hijo)
+
+        poblacion = nueva_poblacion
+        candidato = min(
+            poblacion,
+            key=lambda individuo: fitness_pfsp(individuo, tiempos),
+        )
+
+        if frecuencia_local > 0 and (generacion + 1) % frecuencia_local == 0:
+            mejorado = busqueda_local_insercion(
+                candidato, tiempos, generador, max_evaluaciones_local
+            )
+            for i, individuo in enumerate(poblacion):
+                if np.array_equal(individuo, candidato):
+                    poblacion[i] = mejorado
+                    break
+            candidato = mejorado
+
+        candidato_costo = fitness_pfsp(candidato, tiempos)
+        if candidato_costo < mejor_costo:
+            mejor = candidato.copy()
+            mejor_costo = candidato_costo
+
+        if retornar_historia:
+            historia.append(mejor_costo)
+
+    if retornar_historia:
+        return mejor, mejor_costo, historia
+    return mejor, mejor_costo
