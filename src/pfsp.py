3e9f7fd8e2eb7a08c7c00cac9e6662c81b1a157f
matt <matt@gmail.com>
2026-09-20 00:24:58 -0300

    add: importando correctamente las funciones

--- src/pfsp.py
+++ src/pfsp.py
@@ -4,14 +4,14 @@ import os
 
 # individuos, y poblacion en la asociacion del algiritmo genetico
 def fitness_pfsp(permutacion, tiempos):
-    cantidad_maquinas = tiempos.shape[1]
+    cantidad_maquinas = tiempos.shape[0]
     finalizacion_maquinas = np.zeros(cantidad_maquinas, dtype=np.int64)
 
     for trabajo in permutacion:
         finalizacion_trabajo = np.int64(0)
         for maquina in range(cantidad_maquinas):
             inicio = max(finalizacion_trabajo, finalizacion_maquinas[maquina])
-            finalizacion_trabajo = inicio + tiempos[trabajo, maquina]
+            finalizacion_trabajo = inicio + tiempos[maquina, trabajo]
             finalizacion_maquinas[maquina] = finalizacion_trabajo
 
     return int(finalizacion_maquinas[-1])
